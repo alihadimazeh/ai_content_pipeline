9876d4b8185bc51627bd2827cf7f5d62c1e047da
Ali Hadi Mazeh <alihadimazeh@cmail.carleton.ca>
2026-04-23 12:01:48 -0400

    design tokens and helpers for the views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,30 @@ module ApplicationHelper
     markdown = Redcarpet::Markdown.new(renderer, autolink: true, tables: true, fenced_code_blocks: true)
     markdown.render(text).html_safe
   end
+
+  FORMAT_HUES = {
+    "tweet_thread"     => 200,
+    "linkedin_post"    => 260,
+    "blog_outline"     => 140,
+    "email_newsletter" => 30
+  }.freeze
+
+  def format_hue(format)
+    FORMAT_HUES[format] || 70
+  end
+
+  def time_of_day
+    hour = Time.current.hour
+    if hour < 12 then "morning"
+    elsif hour < 17 then "afternoon"
+    else "evening"
+    end
+  end
+
+  def status_chip(status)
+    label = status.humanize
+    content_tag(:span, class: "status-chip #{status}") do
+      content_tag(:span, "", class: "dot") + label
+    end
+  end
 end
